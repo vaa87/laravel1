089145442993de1ee5352ba3a5dfec3d75b8396c
Andrey V <vinyaykin@gmail.com>
2019-05-26 17:17:02 +0300

    вынос логики в контроллер

--- www/routes/web.php
+++ www/routes/web.php
@@ -1,56 +1,5 @@
 <?php
-use Symfony\Component\HttpFoundation\Request;
-use App\Models\Task;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-
-/**
- * Вывести панель с задачами
- */
-Route::get('/', function () {
-    $tasks = Task::orderBy('created_at', 'asc')->get();
-
-    return view('tasks', [
-        'tasks' => $tasks
-    ]);
-});
-
-/**
- * Добавить новую задачу
- */
-Route::post('/task', function (Request $request) {
-    $validator = Validator::make($request->all(), [
-        'name' => 'required|max:255',
-    ]);
-
-    if ($validator->fails()) {
-        return redirect('/')
-            ->withInput()
-            ->withErrors($validator);
-    }
-
-    $task = new Task;
-    $task->name = $request->name;
-    $task->save();
-
-    return redirect('/');
-});
-
-/**
- * Удалить задачу
- */
-Route::delete('/task/{task}', function (Task $task) {
-    $task->delete();
-
-    return redirect('/');
-});
+Route::get('/', 'TaskController@index');
+Route::post('/task', 'TaskController@add');
+Route::delete('/task/{task}', 'TaskController@delete');
